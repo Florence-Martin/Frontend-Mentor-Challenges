0f31565ca2189f90e3f8383e2576a0eba4b944e5
Florence Martin <florence.martin469@gmail.com>
2025-02-17 18:30:46 +0100

    Chore: Remove qr-code-component to reset deployment

--- apps/qr-code-component/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // Empêche ESLint de bloquer le build
-  },
-};
-
-export default nextConfig;
