d9e86d527ac7f1a9ca40a722c2a4a6fffc13245b
Florence Martin <florence.martin469@gmail.com>
2025-02-20 11:29:23 +0100

    feat: add a new challenge github-user-search-app

---
+++ apps/github-user-search-app/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>🚧 New challenge in progress 🚧</h1>
+      <p>
+        The goal is to build a GitHub user search app using the GitHub API.{" "}
+      </p>
+    </div>
+  );
+}
